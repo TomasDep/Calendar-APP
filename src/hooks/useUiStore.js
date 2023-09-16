0f928c11e7d02269e996f80d6124d5b4ba728be8
TomasDep <tomas.valenzuelacb@gmail.com>
2023-09-15 22:53:58 -0300

    ADD

---
+++ src/hooks/useUiStore.js
@@ -0,0 +1,22 @@
+import { useDispatch, useSelector } from "react-redux";
+import { onCloseDateModal, onOpenDateModal } from "../store";
+
+export const useUiStore = () => {
+  const dispatch = useDispatch();
+  const { isDateModalOpen } = useSelector((state) => state.ui);
+  const openDateModal = () => {
+    dispatch(onOpenDateModal());
+  };
+  const closeDateModal = () => {
+    dispatch(onCloseDateModal());
+  };
+  const toggleDateModal = () => {
+    isDateModalOpen ? openDateModal() : closeDateModal();
+  };
+  return {
+    isDateModalOpen,
+    openDateModal,
+    closeDateModal,
+    toggleDateModal,
+  };
+};
---
+++ src/store/calendar/calendarSlice.js
@@ -0,0 +1,51 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { addHours } from "date-fns";
+
+const tempEvent = {
+  _id: new Date().getTime(),
+  title: "My birthday",
+  notes: "Buying cakes",
+  start: new Date(),
+  end: addHours(new Date(), 2),
+  bgColor: "#faf",
+  user: {
+    _id: "123",
+    name: "TomasDep",
+  },
+};
+
+export const calendarSlice = createSlice({
+  name: "calendar",
+  initialState: {
+    events: [tempEvent],
+    activeEvent: null,
+  },
+  reducers: {
+    onSetActiveEvent: (state, { payload }) => {
+      state.activeEvent = payload;
+    },
+    onAddNewEvent: (state, { payload }) => {
+      state.events.push(payload);
+      state.activeEvent = null;
+    },
+    onUpdateEvent: (state, { payload }) => {
+      state.events = state.events.map((event) => {
+        if (event._id === payload._id) {
+          return payload;
+        }
+        return event;
+      });
+    },
+    onDeleteEvent: (state) => {
+      if (state.activeEvent) {
+        state.events = state.events.filter(
+          (event) => event._id !== state.activeEvent._id
+        );
+        state.activeEvent = null;
+      }
+    },
+  },
+});
+
+export const { onSetActiveEvent, onAddNewEvent, onUpdateEvent, onDeleteEvent } =
+  calendarSlice.actions;
